6717f226e2c05abbcf81c36248433fb8bb3398d2
imdoom <akshay.kumar2021@u.northwestern.edu>
2021-08-31 06:21:54 +0530

    Updates

---
+++ precache-manifest.5587a9a06f85a7c5486bf758f25c8263.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dcc79837924ca2a78db101016fe57d5",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "8a893ce1f0a364487cce",
+    "url": "/shopping-cart/static/css/2.fc65253b.chunk.css"
+  },
+  {
+    "revision": "55005e23bd7336ff462b",
+    "url": "/shopping-cart/static/css/main.2edc2551.chunk.css"
+  },
+  {
+    "revision": "8a893ce1f0a364487cce",
+    "url": "/shopping-cart/static/js/2.33c10549.chunk.js"
+  },
+  {
+    "revision": "2cdc80f2bb8483b086fbc42f20a1a744",
+    "url": "/shopping-cart/static/js/2.33c10549.chunk.js.LICENSE"
+  },
+  {
+    "revision": "55005e23bd7336ff462b",
+    "url": "/shopping-cart/static/js/main.ccef56f7.chunk.js"
+  },
+  {
+    "revision": "295d59a6a91df6e0be68",
+    "url": "/shopping-cart/static/js/runtime-main.741427f3.js"
+  }
+]);
