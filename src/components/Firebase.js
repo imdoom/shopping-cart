4e946fd9229ddf068925bc3c1cbc5c9f4f3a608a
imdoom <akshay.kumar2021@u.northwestern.edu>
2026-04-30 00:39:45 +0530

    app running on local

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -1,28 +1,26 @@
-import firebase from "firebase/app";
-import 'firebase/database';
-import 'firebase/auth';
+import firebase from "firebase/compat/app";
+import "firebase/compat/database";
+import "firebase/compat/auth";
 
 const uiConfig = {
-    signInFlow: 'popup',
-    signInOptions: [
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID
-    ],
-    callbacks: {
-        signInSuccessWithAuthResult: () => false
-    }
+  signInFlow: "popup",
+  signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+  callbacks: {
+    signInSuccessWithAuthResult: () => false,
+  },
 };
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCK8vbymPcLDewFfHznSt8lEdgGZp4O4rk",
-    authDomain: "shopping-cart-2069e.firebaseapp.com",
-    databaseURL: "https://shopping-cart-2069e.firebaseio.com",
-    projectId: "shopping-cart-2069e",
-    storageBucket: "shopping-cart-2069e.appspot.com",
-    messagingSenderId: "17139108491",
-    appId: "1:17139108491:web:b7a7a301853ae0e95f228e",
-    measurementId: "G-NF59H641RN"
+  apiKey: "AIzaSyCK8vbymPcLDewFfHznSt8lEdgGZp4O4rk",
+  authDomain: "shopping-cart-2069e.firebaseapp.com",
+  databaseURL: "https://shopping-cart-2069e.firebaseio.com",
+  projectId: "shopping-cart-2069e",
+  storageBucket: "shopping-cart-2069e.appspot.com",
+  messagingSenderId: "17139108491",
+  appId: "1:17139108491:web:b7a7a301853ae0e95f228e",
+  measurementId: "G-NF59H641RN",
 };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database().ref();
 
-export {db, uiConfig, firebase};
+export { db, uiConfig, firebase };
