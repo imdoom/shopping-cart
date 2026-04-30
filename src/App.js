656d0a5560b8c880ab77ac9bdd97e9bac96341d5
imdoom <akshay.kumar2021@u.northwestern.edu>
2026-04-30 05:35:53 +0530

    cart sync with user login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import "rbx/index.css";
 import "./css/App.css";
 import "./css/ShoppingCart.css";
 import { Column, Button, Title, Message } from "rbx";
 import ShoppingCart from "./components/ShoppingCart";
 import Product from "./components/Product";
-import {db, uiConfig, firebase} from './components/Firebase';
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import { db, uiConfig, firebase } from "./components/Firebase";
+import { set } from "firebase/database";
 
 const App = () => {
   const [data, setData] = useState({});
@@ -16,19 +16,28 @@ const App = () => {
   const [sizes, setSizes] = useState([]);
   const [user, setUser] = useState();
   const [loading, setLoading] = useState(false);
+  const [selectedSize, setSelectedSize] = useState();
+  const [resetSize, setResetSize] = useState(false);
 
   const products = Object.values(data);
   const sizeFilter = () => {
     var arr = [];
     var keys = Object.keys(inventory);
-    sizes.length > 0 ? sizes.forEach(s => {keys.forEach(key => {if(inventory[key][s] > 0 && !arr.find(i => i.sku == key)) arr.push(products.find(p => p.sku == key))})}) : arr = [...products, ];
+    sizes.length > 0
+      ? sizes.forEach((s) => {
+          keys.forEach((key) => {
+            if (inventory[key][s] > 0 && !arr.find((i) => i.sku == key))
+              arr.push(products.find((p) => p.sku == key));
+          });
+        })
+      : (arr = [...products]);
     return arr;
-  }
+  };
   const shownProducts = sizeFilter();
 
   useEffect(() => {
     const fetchProducts = async () => {
-      const response = await fetch('./data/products.json');
+      const response = await fetch("./data/products.json");
       const json = await response.json();
       setData(json);
     };
@@ -36,48 +45,83 @@ const App = () => {
   }, []);
 
   useEffect(() => {
-    firebase.auth().onAuthStateChanged(setUser);
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        console.log("logged in");
+        setUser(user);
+        setResetSize(false);
+        const userCartRef = db.child("carts").child(user.uid);
+        userCartRef
+          .once("value")
+          .then((snap) => {
+            const val = snap.val();
+            if (val) {
+              setCartProducts(val.cartProducts || []);
+              setSizes(val.sizes || []);
+            }
+          })
+          .catch((err) => console.error("Failed to fetch saved cart:", err));
+      } else {
+        setUser(null);
+        setCartProducts([]);
+        setSizes([]);
+        setCartOpen(false);
+        setResetSize(true);
+      }
+    });
   }, []);
- 
+
+  useEffect(() => {
+    if (!user) return;
+    const userCartRef = db.child("carts").child(user.uid);
+    userCartRef
+      .set({
+        cartProducts: cartProducts,
+        sizes: sizes,
+        updatedAt: new Date().toISOString(),
+      })
+      .catch((err) => console.error("Failed to save cart:", err));
+  }, [cartProducts, sizes]);
+
   useEffect(() => {
     setLoading(true);
-    const handleData = snap => {
+    const handleData = (snap) => {
       if (snap.val()) setInventory(snap.val());
-    }
-    db.on('value', handleData, error => alert(error));
-    return () => { db.off('value', handleData); };
+    };
+    db.on("value", handleData, (error) => alert(error));
+    return () => {
+      db.off("value", handleData);
+    };
   }, []);
 
   const addProduct = (product) => {
     let products = [...cartProducts];
     let newProd;
-    const index = products.findIndex(i => i.sku === product.sku);
+    const index = products.findIndex((i) => i.sku === product.sku);
     //console.log("index", index);
-    if(index>-1){
-      newProd = {...products[index], quantity: products[index].quantity + 1};
+    if (index > -1) {
+      newProd = { ...products[index], quantity: products[index].quantity + 1 };
       products.splice(index, 1, newProd);
-    }
-    else{
-      newProd = {...product, quantity: 1};
+    } else {
+      newProd = { ...product, quantity: 1 };
       products.push(newProd);
     }
     //console.log("After adding new prod", products);
     setCartProducts(products);
-  }
-  const sizeSelection = size => {
+  };
+  const sizeSelection = (size) => {
     var arr = [...sizes];
     const index = arr.indexOf(size);
-    if(index > -1){
+    if (index > -1) {
       arr.splice(index, 1);
-    }
-    else{
+    } else {
       arr.push(size);
     }
     setSizes(arr);
-  }
+  };
   const Banner = ({ user }) => (
     <React.Fragment>
-      { user ? <Welcome user={ user } /> : <SignIn /> }
+      {user ? <Welcome user={user} /> : <SignIn />}
     </React.Fragment>
   );
   const Welcome = ({ user }) => (
@@ -91,53 +135,98 @@ const App = () => {
     </Message>
   );
   const SignIn = () => (
-    <StyledFirebaseAuth
-      uiConfig={uiConfig}
-      firebaseAuth={firebase.auth()}
-    />
+    <Button
+      primary
+      onClick={() =>
+        firebase
+          .auth()
+          .signInWithPopup(new firebase.auth.GoogleAuthProvider(setUser))
+      }
+    >
+      Sign in with Google
+    </Button>
   );
 
   return (
     <div id="app">
       <Column.Group>
         <Column textAlign="centered" size={2}>
-          <Title >Filter Sizes</Title>
+          <Title>Filter Sizes</Title>
         </Column>
         <Column size={2}>
           <Button.Group textAlign="left" narrow>
-            <Button rounded onClick={() => sizeSelection('S')} color={sizes.includes('S') ? "black" : "" } className = "sizeBtn">S</Button>
-            <Button rounded onClick={() => sizeSelection('M')} color={sizes.includes('M') ? "black" : ""} className = "sizeBtn">M</Button>
-            <Button rounded onClick={() => sizeSelection('L')} color={sizes.includes('L') ? "black" : ""} className = "sizeBtn">L</Button>
-            <Button rounded onClick={() => sizeSelection('XL')} color={sizes.includes('XL') ? "black" : ""} className = "sizeBtn">XL</Button>
+            <Button
+              rounded
+              onClick={() => sizeSelection("S")}
+              color={sizes.includes("S") ? "black" : ""}
+              className="sizeBtn"
+            >
+              S
+            </Button>
+            <Button
+              rounded
+              onClick={() => sizeSelection("M")}
+              color={sizes.includes("M") ? "black" : ""}
+              className="sizeBtn"
+            >
+              M
+            </Button>
+            <Button
+              rounded
+              onClick={() => sizeSelection("L")}
+              color={sizes.includes("L") ? "black" : ""}
+              className="sizeBtn"
+            >
+              L
+            </Button>
+            <Button
+              rounded
+              onClick={() => sizeSelection("XL")}
+              color={sizes.includes("XL") ? "black" : ""}
+              className="sizeBtn"
+            >
+              XL
+            </Button>
           </Button.Group>
         </Column>
         <Column paddingless marginless>
-          <Banner user={ user }/>
+          <Banner user={user} />
         </Column>
         <Column textAlign="right" size={2}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="45px"
-            height="45px" 
-            viewBox="0 0 24 24" 
-            onClick={() => { setCartOpen(!cartOpen)}}
+            height="45px"
+            viewBox="0 0 24 24"
+            onClick={() => {
+              setCartOpen(!cartOpen);
+            }}
             className="svgIcon"
           >
-            <path fill="currentColor" d="M17,18C15.89,18 15,18.89 15,20A2,2 0 0,0 17,22A2,2 0 0,0 19,20C19,18.89 18.1,18 17,18M1,2V4H3L6.6,11.59L5.24,14.04C5.09,14.32 5,14.65 5,15A2,2 0 0,0 7,17H19V15H7.42A0.25,0.25 0 0,1 7.17,14.75C7.17,14.7 7.18,14.66 7.2,14.63L8.1,13H15.55C16.3,13 16.96,12.58 17.3,11.97L20.88,5.5C20.95,5.34 21,5.17 21,5A1,1 0 0,0 20,4H5.21L4.27,2M7,18C5.89,18 5,18.89 5,20A2,2 0 0,0 7,22A2,2 0 0,0 9,20C9,18.89 8.1,18 7,18Z" />
+            <path
+              fill="currentColor"
+              d="M17,18C15.89,18 15,18.89 15,20A2,2 0 0,0 17,22A2,2 0 0,0 19,20C19,18.89 18.1,18 17,18M1,2V4H3L6.6,11.59L5.24,14.04C5.09,14.32 5,14.65 5,15A2,2 0 0,0 7,17H19V15H7.42A0.25,0.25 0 0,1 7.17,14.75C7.17,14.7 7.18,14.66 7.2,14.63L8.1,13H15.55C16.3,13 16.96,12.58 17.3,11.97L20.88,5.5C20.95,5.34 21,5.17 21,5A1,1 0 0,0 20,4H5.21L4.27,2M7,18C5.89,18 5,18.89 5,20A2,2 0 0,0 7,22A2,2 0 0,0 9,20C9,18.89 8.1,18 7,18Z"
+            />
           </svg>
           {/* <Button  id="shop-cart" size ="large"><i className="material-icons">shopping_cart</i></Button> */}
-        </Column>        
-      </Column.Group>     
-      <ShoppingCart shopCartState={{cartOpen, setCartOpen}} productState={{cartProducts, setCartProducts}}>Cart</ShoppingCart>
-      
+        </Column>
+      </Column.Group>
+      <ShoppingCart
+        shopCartState={{ cartOpen, setCartOpen }}
+        productState={{ cartProducts, setCartProducts }}
+      >
+        Cart
+      </ShoppingCart>
+
       <Column.Group multiline centered gapSize={5} className="cards">
-        {shownProducts.map(product => ( 
+        {shownProducts.map((product) => (
           <Product
             key={product.sku}
-            product={product} 
-            inventory={inventory} 
-            setCartOpen={setCartOpen} 
+            product={product}
+            inventory={inventory}
+            setCartOpen={setCartOpen}
             addProduct={addProduct}
+            resetSize={resetSize}
           />
         ))}
       </Column.Group>
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,51 +1,68 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Column, Notification, Image, Button, Title } from "rbx";
 import "../css/App.css";
 
-const Product = ({key, product, inventory, setCartOpen, addProduct}) => {
-    const [sizeError, setSizeError] = useState();
-    const [selectedSize, setSelectedSize] = useState();
+const Product = ({
+  key,
+  product,
+  inventory,
+  setCartOpen,
+  addProduct,
+  resetSize,
+}) => {
+  const [sizeError, setSizeError] = useState();
+  const [selectedSize, setSelectedSize] = useState();
 
-    return (
-        <Column key={key} size="one-quarter">
-            <Notification color="white" textAlign="centered">
-                <Image.Container size="128">
-                <Image
-                    src={`/data/products/${product.sku}_1.jpg`}
-                />
-                </Image.Container>
-                {product.title}
-                <Title size={4}>${product.price}</Title>
-                <Button.Group 
-                align="centered" 
-                badge={sizeError}
-                badgeColor="danger">
-                {Object.keys(inventory).length > 0 ? ( 
-                    Object.keys(inventory[product.sku]).filter(s => inventory[product.sku][s] > 0).map(s =>
-                    <Button
+  return (
+    <Column key={key} size="one-quarter">
+      <Notification color="white" textAlign="centered">
+        <Image.Container size="128">
+          <Image src={`/data/products/${product.sku}_1.jpg`} />
+        </Image.Container>
+        {product.title}
+        <Title size={4}>${product.price}</Title>
+        <Button.Group align="centered" badge={sizeError} badgeColor="danger">
+          {Object.keys(inventory).length > 0
+            ? Object.keys(inventory[product.sku])
+                .filter((s) => inventory[product.sku][s] > 0)
+                .map((s) => (
+                  <Button
                     className="sizeBtn"
-                    onClick={() => { setSizeError(null); selectedSize === s ? setSelectedSize(null) : setSelectedSize(s);}}
-                    color = {selectedSize && selectedSize === s ? "black" : ""}>
-                        {s}
-                    </Button>)
-                    ) :
-                    null
-                }
-                <Button fullwidth color="black" size="large" onClick={() => {
-                    if(selectedSize) {
-                    setCartOpen(true); 
-                    addProduct(product);
+                    onClick={() => {
+                      setSizeError(null);
+                      selectedSize === s
+                        ? setSelectedSize(null)
+                        : setSelectedSize(s);
+                    }}
+                    color={
+                      selectedSize && !resetSize && selectedSize === s
+                        ? "black"
+                        : ""
                     }
-                    else {
-                    setSizeError("Select size");
-                    }
-                }}>
-                    Add to cart
-                </Button>
-                </Button.Group>
-            </Notification>
-        </Column>
-    );
+                  >
+                    {s}
+                  </Button>
+                ))
+            : null}
+          <Button
+            fullwidth
+            color="black"
+            size="large"
+            onClick={() => {
+              if (selectedSize) {
+                setCartOpen(true);
+                addProduct(product);
+              } else {
+                setSizeError("Select size");
+              }
+            }}
+          >
+            Add to cart
+          </Button>
+        </Button.Group>
+      </Notification>
+    </Column>
+  );
 };
 
 export default Product;
